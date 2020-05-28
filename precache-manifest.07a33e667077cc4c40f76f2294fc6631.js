535eb7d08082ecea1cef64591c6b1eb3e348b96f
lisbeth mbuvi <lisbeth.mbuvi@mawingunetworks.com>
2020-05-28 18:15:32 +0300

    Updates

---
+++ precache-manifest.07a33e667077cc4c40f76f2294fc6631.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd5cc496d6787214e5c3013f2c5e333",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "bdd66f5260ecde0ca319",
+    "url": "/Covid-19-Tracker/static/css/main.c2e6ebdb.chunk.css"
+  },
+  {
+    "revision": "e0f5816a8158f9711b4b",
+    "url": "/Covid-19-Tracker/static/js/2.457fc2be.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Tracker/static/js/2.457fc2be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd66f5260ecde0ca319",
+    "url": "/Covid-19-Tracker/static/js/main.508ee236.chunk.js"
+  },
+  {
+    "revision": "4c2ee1f7cfe5ccc413f5",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.a27bc7e1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-Tracker/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Covid-19-Tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
